a3c300d9d32af492ee9f9518ed9fcb4f93514b36
ArtyomVorontsov <artv20022@gmail.com>
2023-01-08 14:17:31 +0000

    Exercise 14 improved

--- chapter8/programmingProjects/exercise14.c
+++ chapter8/programmingProjects/exercise14.c
@@ -3,7 +3,7 @@
 
 int main(void){
 	char sentence[MAX_SENTENCE_LENGTH] = {0};
-	int i = 0, j = 0, k = 0, s_length, w_start, w_end, word_count;
+	int i = 0, j = 0, s_length, w_start, w_end;
 	
 	printf("Enter a sentence: ");
 	// Read sentence 	
@@ -16,16 +16,25 @@ int main(void){
 		if(sentence[i] == ' ' || i == s_length){
 
 			// Determine word start and word end
-			for(j = w_end; sentence[j] != ' ' && j > 0; j--, k++);
+			for(j = w_end; sentence[j] != ' ' && j > 0; j--);
 			w_start = j;
 		
 			// Print word	
 			for(; j <= w_end; j++)
-				if(sentence[j] != '?' && sentence[j] != ' ') putchar(sentence[j]);
-			
+				if(
+					sentence[j] != '?' && 
+					sentence[j] != ' ' && 
+					sentence[j] != '!'
+				) putchar(sentence[j]);
+		
+			// Set next word end	
 			w_end = w_start - 1;
 
-			sentence[i] == '?' ? putchar('?') : putchar(' ');
+			switch(sentence[i]){
+				case '?': putchar('?'); break;
+				case '!': putchar('!'); break;
+				default: putchar(' '); break;
+			}
 		}
 	}
 	putchar('\n');	
